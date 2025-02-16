cf696ed7a3084f76023ec10199421c0ff3727b21
lhoju0158 <lhoju0158@gmail.com>
2025-02-16 17:15:24 +0900

    [Silver IV] Title: 수 찾기, Time: 84 ms, Memory: 7168 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, m;
+int b[100004];
+map<int,int> mp;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int tem;
+		cin >> tem;
+		mp.insert({ tem,1 });
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++)cin >> b[i];
+	for (int i = 0; i < m; i++) {
+		if (mp.find(b[i]) != mp.end()) cout << 1;
+		else cout << 0;
+		cout << "\n";
+	}
+
+	return 0;
+}
