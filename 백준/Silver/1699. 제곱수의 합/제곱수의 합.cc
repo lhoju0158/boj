03a54c5a18c78facc1e4fcbda0f827e90c6a52f3
lhoju0158 <lhoju0158@gmail.com>
2025-03-01 16:10:11 +0900

    [Silver II] Title: 제곱수의 합, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int dp[100004];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	for (int i = 1; i < n + 1; i++) {
+		dp[i] = i;
+		for (int j = 1; j * j <= i; j++) {
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+		}
+	}
+	cout << dp[n];
+	return 0;
+}
